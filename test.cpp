bd13494d5e9e5f37dbe04c58221253f79a45c47b
Kollár András <146661149+kollarandras03@users.noreply.github.com>
2025-04-28 08:49:08 +0200

    Create test.cpp
    added a sample cpp file for adding big nums

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+
+const int NUM_SMALL = 100000;
+const int NUM_BIG = 1000000;
+
+// Multiplies 2 potentially big numbers safely
+long long prodLargeNums(const int num1, const int num2){
+    long long product = num1 * num2;
+    return product;
+}
+
+int main(){
+    std::cout << "The product of the numbers are: ";
+    std::cout << prodLargeNums(NUM_SMALL, NUM_BIG) << std::endl;
+    return 0;
+}
